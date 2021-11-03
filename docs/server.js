77a02433e5a1b44c1258851bbbbda4ac4544c290
Wilker Machado <wilkerjmachado@gmail.com>
2021-11-03 09:47:14 -0300

    Update docs/server.js

--- docs/server.js
+++ docs/server.js
@@ -1,8 +1,12 @@
 module.exports = {
     servers: [
         {
+            url: "https://w-nodejs-api.herokuapp.com/api",
+            description: "Heroku server",
+        },
+        {
             url: "http://localhost:8080/api", // url
             description: "Local server", // name
-        },
+        }
     ],
 };
